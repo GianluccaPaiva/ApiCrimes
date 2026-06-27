acdd2d700e3156333dfd91deb0f02c0354f552fb
GianluccaPaiva <gianluccapaiva@gmail.com>
2026-06-27 18:29:18 -0300

    adição de mais dados e correção do get

--- index.js
+++ index.js
@@ -6,17 +6,66 @@ const app = express();
 app.use(express.json());
 
 app.get('/crimes', (req, res) => {
-    const  filtroComum  = req.query.crimeComum;
-    const isComum = filtroComum === 'true' ;
-    if(filtroComum !== undefined) {
-        const crimesComum = crimes.filter(crime => crime.crimeComum === isComum);
-        return res.json(crimesComum);
+    const filtro = req.query;
+
+    if(Object.keys(filtro).length === 0) {
+        return res.json(crimes);
     }
-    return res.json(crimes);
+
+    let crimesFiltrados = crimes;
+    
+    for(let chave in filtro) {
+        let valorDeBusca = filtro[chave];
+        
+        if (valorDeBusca === "true") {
+            valorDeBusca = true;
+        } else if (valorDeBusca === "false") {
+            valorDeBusca = false;
+            
+        } 
+       
+        else if (chave === 'id') {
+            valorDeBusca = Number(valorDeBusca);
+        }
+     
+        else if (typeof valorDeBusca === 'string') {
+            valorDeBusca = valorDeBusca.toLowerCase();
+        }
+
+        crimesFiltrados = crimesFiltrados.filter(crime => {
+            
+            let valorDoCrime;
+         
+            if (chave === 'bairro' || chave === 'cidade' || chave === 'ponto_referencia') {
+                valorDoCrime = crime.localizacao[chave];
+            } else {
+                valorDoCrime = crime[chave];
+            }
+
+      
+            if (valorDoCrime === undefined || valorDoCrime === null) {
+                return false;
+            }
+
+            if (typeof valorDoCrime === 'string') {
+                valorDoCrime = valorDoCrime.toLowerCase();
+                
+                if (chave === 'incidente' || chave === 'ponto_referencia') {
+                    return valorDoCrime.includes(valorDeBusca); 
+                }
+            }
+
+       
+            return valorDoCrime === valorDeBusca;
+        });
+    }
+
+    return res.json(crimesFiltrados);
 });
 
 let PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
     console.log(`Acesse: http://localhost:${PORT}/crimes`);
-});
+})
+
