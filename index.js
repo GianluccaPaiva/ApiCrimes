8388145aa2003724d9307db169a63c0db4b062f7
GianluccaPaiva <gianluccapaiva@gmail.com>
2026-06-27 18:42:20 -0300

    resposta ao defalut /

--- index.js
+++ index.js
@@ -63,6 +63,11 @@ app.get('/crimes', (req, res) => {
     return res.json(crimesFiltrados);
 });
 
+// Rota principal (Boas-vindas)
+app.get('/', (req, res) => {
+    res.send("🕷️ Bem-vindo à API do Sistema de Alerta de Juiz de Fora! Adicione '/crimes' ao final do URL para ver as ocorrências.");
+});
+
 let PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
